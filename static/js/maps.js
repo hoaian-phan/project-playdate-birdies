25a41fd2f6f7918f70aa19e911665291f3fd730b
Hoai An Phan <pnhoaian2904@gmail.com>
2022-03-28 05:03:53 +0000

    Added autocomplete for the address of the event location in hosting form

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,7 +1,10 @@
 'use strict';
 
-// Display a map and mark locations on the map
+// This function uses: 1. autocomplete searchbox to suggest the full address of the location,
+// 2. geocoding to get location's coordinates 
+// 3. textSearch to get location's photo
 function otherMap() {
+    // Create a basicMap, which will not be displayed on the page
     const basicMap = new google.maps.Map(document.querySelector('#map'), {
         center: {
             lat: 37.601773,
@@ -9,6 +12,60 @@ function otherMap() {
         },
         zoom: 11,
     });
+
+    // 1. Use Autocomplete Address Form to populate location's full address
+    let addressField = document.getElementById("address");
+    let postalField = document.getElementById("zipcode");
+    // Create the autocomplete object, restricting the search predictions to addresses in the US 
+    const autocomplete = new google.maps.places.Autocomplete(addressField, {
+        componentRestrictions: { country: "us" },
+        fields: ["address_components", "geometry", "photo"],
+        types: ["address"],
+    });
+    addressField.focus();
+    // When the user selects an address from the drop-down: .
+    autocomplete.addListener("place_changed", () => {
+        // 1. Populate the address fields to fill in the rest of the address form
+        // Get the place details from the autocomplete object
+        const place = autocomplete.getPlace();
+        let address = "";
+        let postcode = "";
+        // Get each component of the address from the place details, and fill-in the corresponding field on the form.
+        for (const component of place.address_components) {
+            const componentType = component.types[0];
+
+            switch (componentType) {
+                case "street_number": {
+                    address = `${component.long_name} ${address}`;
+                    break;
+                }
+                case "route": {
+                    address += component.short_name;
+                    break;
+                }
+                case "postal_code": {
+                    postcode = `${component.long_name}${postcode}`;
+                    break;
+                }
+                case "postal_code_suffix": {
+                    postcode = `${postcode}-${component.long_name}`;
+                    break;
+                }
+                case "locality": {
+                    document.querySelector("#city").value = component.long_name;
+                    break;
+                }
+                case "administrative_area_level_1": {
+                    document.querySelector("#state").value = component.short_name;
+                    break;
+                }
+            }
+        }
+        addressField.value = address;
+        postalField.value = postcode;
+    });
+
+
     // Geocode the location when host submits the hosting form
     // Select the form and add event handler
     document.getElementById("host_form").addEventListener("submit", () => {
@@ -84,12 +141,5 @@ function otherMap() {
         
         
             
-    })
-
-
-
-
-    
-
-    
+    })  
 }
